ef16f0731e11fea61960354a292a9d138464891e
Ketzel-GV <zs24013346@estudiantes.uv.mx>
2026-05-08 11:28:26 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -9,37 +9,57 @@
 // let b9 = document.getElementById("b9")
 // let b0 = document.getElementById("b0")
 
-let resultado = document.getElementById("resultado")
-let botones = document.getElementsByTagName("button")
-for (const key in botones) {
-    if (Object.prototype.hasOwnProperty.call(botones, key)){
-        const boton = botones[key];
-        if(boton.className != "operadores")
-            boton.addEventListener("click", pintar)
-        else
-            boton.addEventListener("click", pintar2)
-        //console.log(boton)
+let resultado = document.getElementById("resultado");
+let botones = document.getElementsByTagName("button");
 
+let valorAnterior = "";
+let operacionActual = null;
+
+
+for (let boton of botones) {
+    if (boton.id === "eq") {
+        boton.addEventListener("click", calcular);
+    } else if (boton.id === "lim") {
+        boton.addEventListener("click", limpiar);
+    } else if (boton.classList.contains("operador")) {
+        boton.addEventListener("click", prepararOperacion);
+    } else {
+        boton.addEventListener("click", agregarNumero);
     }
 }
 
-function pintar(e){
-    console.log(e.target.innerText)
-    resultado.value += e.target.innerText
+function agregarNumero(e) {
+    resultado.value += e.target.innerText;
 }
 
-function pintar2(e){
-    console.log(e.target.innerText)
-    if(e.target.innerText == "+")
-        suma()
+function prepararOperacion(e) {
+    valorAnterior = resultado.value;
+    operacionActual = e.target.innerText;
+    resultado.value = ""; 
 }
 
-let prm1
-function suma(){
-    prm1 = resultado.value = ""
+function limpiar() {
+    resultado.value = "";
+    valorAnterior = "";
+    operacionActual = null;
 }
 
-function igual(){
-    prm2 = resultado.value
-    resultado.value = parseInt(prm1) + parseInt(prm2)
+function calcular() {
+    if (operacionActual === null || valorAnterior === "") return;
+
+    let p1 = parseFloat(valorAnterior);
+    let p2 = parseFloat(resultado.value);
+    let final = 0;
+
+    switch (operacionActual) {
+        case "+": final = p1 + p2; break;
+        case "-": final = p1 - p2; break;
+        case "*": final = p1 * p2; break;
+        case "/": 
+            final = p2 !== 0 ? p1 / p2 : "Error"; 
+            break;
+    }
+
+    resultado.value = final;
+    operacionActual = null; 
 }
